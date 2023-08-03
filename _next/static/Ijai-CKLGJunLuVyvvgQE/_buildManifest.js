77e75ad65d30815c31dd8d0a3367c1c56946067a
s1f102103796 <s1f102103796@users.noreply.github.com>
2023-08-03 16:18:27 +0000

    deploy: c68dc9c3f1bd960003951d85ea42816fdf0bb10f

---
+++ _next/static/Ijai-CKLGJunLuVyvvgQE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/317f54c887fdbec4.css","static/chunks/pages/index-f03af0b08cb99b4a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
