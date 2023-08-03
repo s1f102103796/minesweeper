c68dc9c3f1bd960003951d85ea42816fdf0bb10f
s1f102103796 <85663261+s1f102103796@users.noreply.github.com>
2023-08-04 01:17:02 +0900

    minesweeperで押したところが捲れる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,134 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [userInputs, setUserInputs] = useState<(0 | 1)[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  const bombCount = 10;
+  const [bombMap, setBombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  const directions = [
+    [-1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+  ];
+
+  //開始された合図
+  const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+  //爆発した合図
+  const isFaillure = userInputs.some((row, y) =>
+    row.some((input, x) => input === 1 && bombMap[y][x] === 1)
+  );
+  let row = 0;
+  let col = 0;
+  let bomb = 0;
+  let cnt = 0;
+  //input = 初めて押した時
+  const jaj = userInputs.flat().filter((input) => input === 1).length === 0;
+
+  //自分の状態を表すuserとbomb
+  const board = [
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+  ];
+  console.log('初期');
+  console.table(userInputs);
+
+  const createBoard = () => {
+    for (let py = 0; py < 8; py++) {
+      for (let px = 0; px < 8; px++) {
+        if (userInputs[py][px] === 1) {
+          for (let s = 0; s < 8; s++) {
+            const dx = directions[s][0];
+            const dy = directions[s][1];
+            // console.log(dx, dy);
+            if (userInputs[py + dy] !== undefined && bombMap[py + dy][px + dx] === 1) {
+              cnt++;
+            }
+          }
+          board[py][px] = cnt;
+          console.log('updateInputのbombの個数', cnt);
+          console.table(board);
+
+          return board;
+        }
+      }
+    }
+  };
+
+  const onClick = (x: number, y: number) => {
+    console.log('選択', y, x);
+    const updateInput = [...userInputs];
+    updateInput[x][y] = 1;
+
+    setUserInputs(updateInput);
+    console.log('update');
+    console.table(updateInput);
+    if (jaj) {
+      if (updateInput[x][y] === 1) {
+        while (bomb < bombCount) {
+          row = Math.floor(Math.random() * 9);
+          col = Math.floor(Math.random() * 9);
+          console.log(row, col);
+          if (updateInput[row][col] !== 1 && bombMap[row][col] !== 1) {
+            bombMap[row][col] = 1;
+            bomb++;
+          }
+        }
+      }
+    }
+    console.log('bombmap');
+    console.table(bombMap);
+  };
+  createBoard();
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.board}>
+        {board.map((row, x) =>
+          row.map((cell, y) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
+              {cell === -1 && <div className={styles.stone} />}
+              {cell === 0 && <div className={styles.stone0} />}
+              {cell > 0 && cell < 9 && (
+                <div className={styles.number} style={{ backgroundPosition: -30 * cell + 30 }} />
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
